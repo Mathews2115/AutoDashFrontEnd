c00b93860cedd7f75c50eef2923fe696e45f58e1
Michael Mathews II <cloroxman@gmail.com>
2021-02-13 19:03:43 -0800

    Initial Boiler Plate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, ".", "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+};
