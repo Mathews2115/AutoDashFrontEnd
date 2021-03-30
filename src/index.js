2340447b8d3f1b75c9d50ff6b20e785e2695fdd6
Michael Mathews II <cloroxman@gmail.com>
2021-03-29 21:05:14 -0700

    update to complete boiler plate starter pack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+// Test import of a JavaScript function
+//import { example } from './js/example'
---
+++ src/js/example.js
@@ -0,0 +1,6 @@
+export const example = () => {
+  return (
+    'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
+    'with a hot dev server and an optimized production build.'
+  )
+}
