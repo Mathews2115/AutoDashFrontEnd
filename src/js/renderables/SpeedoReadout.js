178474b45cfa268b893726c235272d5e9f5754a0
Michael Mathews II <cloroxman@gmail.com>
2021-05-20 16:49:51 -0700

    render the numbers to textures; dont bother wasting CPU time applying a filter on a static thing each frame

--- src/js/renderables/SpeedoReadout.js
+++ src/js/renderables/SpeedoReadout.js
@@ -6,6 +6,90 @@ import { DATA_KEYS } from "../dataMap";
 import Renderable from "./Renderable";
 import { RENDER_KEYS } from "./Renderables";
 
+const FILTER_PADDING = 20;
+const PADDING_OFFSET = FILTER_PADDING/2;
+/**
+ * Generate the Graphics needed for segments...
+ * lmao im doing this the hard way arent i
+ * @param {Number} number - The number to create out of segments
+ * @param {Number} gaugeHeight - Needed to generate the segments
+ * @returns {PIXI.Graphics}
+ */
+const createNumber = (number,gaugeHeight) => {
+  const segmentLength = gaugeHeight/2 - 15;
+  const segmentWidth = 20;
+  const chamfer = 30;
+  const inset = Math.min(chamfer, Math.min(segmentWidth, segmentLength) / 2);
+  const insetPadding = 3;
+  const middleSegmentPadding = (insetPadding*3)+inset; // padding the verticals use to fit the middle horizontal in there
+  
+  const graphics = new PIXI.Graphics();
+  graphics.beginFill(0xffffff).lineStyle(0);
+  
+  // upper left vertical
+  if (number != 1 && number != 2 && number != 3 && number != 7) {
+    graphics.drawChamferRect(
+      0, inset + insetPadding,
+      segmentWidth, segmentLength,
+      chamfer
+    );
+  }
+
+  // lower left vertical
+  if (number == 8 || number == 6 || number == 2 || number === 0) {
+    graphics.drawChamferRect(
+      0, middleSegmentPadding + segmentLength,
+      segmentWidth, segmentLength,
+      chamfer
+    );
+  }
+
+  // upper right vertical
+  if (number != 5 && number != 6) {
+    graphics.drawChamferRect(
+      segmentLength, inset + insetPadding,
+      segmentWidth, segmentLength,
+      chamfer
+    );
+  }
+
+  // loewr right vertical
+  if (number != 2) {
+    graphics.drawChamferRect(
+      segmentLength,  middleSegmentPadding + segmentLength,
+      segmentWidth, segmentLength,
+      chamfer
+    );
+  }
+
+  // upper horizontals
+  if (number != 4 && number != 1) {
+    graphics.drawChamferRect(inset, 0, segmentLength, segmentWidth, chamfer);
+  }
+
+  // middle horizontal
+  if (number != 0 && number != 1 && number != 7) {
+    graphics.drawChamferRect(
+      inset,          (insetPadding*2) + segmentLength,
+      segmentLength,  segmentWidth,
+      chamfer
+    );
+  }
+
+  // lower horizontal
+  if (number != 1 && number != 4 && number != 7) {
+    graphics.drawChamferRect(
+      inset, inset + 2 + segmentLength + segmentLength,
+      segmentLength, segmentWidth,
+      chamfer
+    );
+  }
+
+  graphics.endFill();
+  return graphics;
+};
+
+const SEGMENT_SLANT = -0.2;
 const ID = RENDER_KEYS.SPEEDO_READOUT;
 class SpeedoReadout extends Renderable {
   constructor({ renderer, theme }) {
@@ -15,6 +99,8 @@ class SpeedoReadout extends Renderable {
     this._value = SPEEDO_CONFIG.MAX;
     this.renderedValue = this._value;
     this.bgSprite = null;
+    this.numberTextures = [];
+    this.numberSprites = [];
   }
 
   // the data store values we want to listen too
@@ -41,50 +127,64 @@ class SpeedoReadout extends Renderable {
   // MAGIC NUMBERS AHOY LOL - so much pixel chooching.  These numbers also depending on
   // renderhelpers.js placement.  Maybe I'll come back and make this less dumb...but I doubt it! :D :D :D    :|
   initialize() {
-    const segmentLength = this.gaugeHeight/2 - 15;
-    const segmentWidth = 20;
-    const chamfer = 30;
-    const inset = Math.min(chamfer, Math.min(segmentWidth, segmentLength) / 2);
-    const insetPadding = 3;
-    const middleSegmentPadding = (insetPadding*3)+inset; // padding the verticals use to fit the middle horizontal in there
+    // TODO: if we call init after it is already initialized; delete ALL textures
 
-    
-    const numberBackground = new PIXI.Graphics();
-    numberBackground.beginFill(0xffffff).lineStyle(0)
-      // left verticals
-      numberBackground.drawChamferRect(0,inset+insetPadding, segmentWidth, segmentLength, chamfer)
-      numberBackground.drawChamferRect(0,middleSegmentPadding+segmentLength, segmentWidth, segmentLength, chamfer)
+    const numberBackground = createNumber(8, this.gaugeHeight);
+
+    // create each number texture
+    for (let i = 0; i <= 10; i++) {
+      const digitContainer = new PIXI.Container();
+
+      // background
+      const background = new PIXI.Graphics(numberBackground.geometry);
+      background.tint = this.theme.gaugeBgColor;
+      digitContainer.addChild(background);
       
-      // right verticals
-      numberBackground.drawChamferRect(segmentLength,inset+insetPadding, segmentWidth, segmentLength, chamfer)
-      numberBackground.drawChamferRect(segmentLength,middleSegmentPadding+segmentLength, segmentWidth, segmentLength, chamfer)
-      
-      // horizontals
-      numberBackground.drawChamferRect(inset,0, segmentLength, segmentWidth, chamfer)
-      numberBackground.drawChamferRect(inset,(insetPadding*2)+segmentLength, segmentLength, segmentWidth, chamfer)
-      numberBackground.drawChamferRect(inset,inset+2+segmentLength+segmentLength, segmentLength, segmentWidth, chamfer)
+      if (i != 10) {
+        // greate number graphics
+        const numberGraphics = createNumber(i, this.gaugeHeight);
+        numberGraphics.tint = this.theme.gaugeActiveColor;
 
-      numberBackground.endFill();
+        // make the digit GLOW like everything else
+        numberGraphics.filters = [new GlowFilter({
+          distance: 20, 
+          outerStrength: 1,
+          innerStrength: 0,
+          color: this.theme.gaugeActiveColor,
+          quality: 0.2,
+        })];
 
-    numberBackground.tint = this.theme.gaugeBgColor;
+        digitContainer.addChild(numberGraphics)
+      }
 
-    // second number
-    const numberBackground2 = new PIXI.Graphics(numberBackground.geometry);
-    numberBackground2.x = numberBackground.width + SCREEN.PADDING;
-    numberBackground2.tint = this.theme.gaugeBgColor;
+      // we need room for the glow before we render this to the texture...
+      // so lets add some padding - there is probably a better way of doing this but whatevs, here we are
+      const container = new PIXI.Container();
+      const emptySpace = new PIXI.Graphics();
+      emptySpace.drawRect(0,0, digitContainer.width+FILTER_PADDING, digitContainer.height+FILTER_PADDING);
+      digitContainer.x = PADDING_OFFSET;
+      digitContainer.y = PADDING_OFFSET;
+      container.addChild(emptySpace, digitContainer);
 
-    // group the numbers together
-    const bg = new PIXI.Container();
-    bg.addChild(numberBackground, numberBackground2);
-    
-    // render background numbers to texture and destroy graphics object
-    const bgTex = this.appRenderer.generateTexture(bg);
-    this.bgSprite = new PIXI.Sprite(bgTex);
+      // bake everything into a texture (background, glowing digit, blank border)
+      this.numberTextures.push(this.appRenderer.generateTexture(container));
+      container.destroy(true); // clean up
+    }
+  
+    // create number textures
+    this.numberSprites.push(new PIXI.Sprite(this.numberTextures[10]));
+    this.numberSprites.push(new PIXI.Sprite(this.numberTextures[5]));
 
-    // give it a slight slant
-    this.bgSprite.setTransform(0,0,1,1,0, -0.2, 0,0,0);
-    bg.destroy(true);
-    this.addChild(this.bgSprite);
+    this.numberSprites.forEach(sprite => {
+      // give the numbers a slight and offset for the extra border we gave the textures
+      sprite.setTransform(-(PADDING_OFFSET),-(PADDING_OFFSET),1,1,0, SEGMENT_SLANT, 0,0,0) 
+    });
+
+    // move second number into place
+    this.numberSprites[1].x = this.numberSprites[1].x + numberBackground.width + SCREEN.PADDING;
+
+    this.addChild(this.numberSprites[0], this.numberSprites[1]);
+    numberBackground.destroy(true); // cleanup
   }
 
   update() {
