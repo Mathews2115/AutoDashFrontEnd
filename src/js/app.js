7ce7689cab4482837d735879ac452bdf130065a0
Michael Mathews II <cloroxman@gmail.com>
2022-02-27 10:39:57 -0800

    cleanup fps counter

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,7 @@ import * as PIXI from "pixi.js";
 import layoutManager from "./layoutManager";
 import { DATA_KEYS, WARNING_KEYS } from "./common/dataMap";
 import { GlitchFilter } from "@pixi/filter-glitch";
-
-// Temporary FPS garbage; i'll actually add this in later (toggalable obviously)
-// const DEFAULT_FONT_SIZE = 30;
-// const DEFAULT_FONT_COLOR = 0xff0000;
-// const _fpsTextField= PIXI.Text;
-// const _fpsTicker=  PIXI.Ticker;
-// let _timeValues= []
-// let _lastTim= null;
+// import FPSTextField from "./renderables/FPSTextField";
 
 const glitchFilter = new GlitchFilter({
   slices: 10,
@@ -38,44 +31,12 @@ export class DashApp {
     this.stage.interactiveChildren = false; // dont bother checking anyone for interactions
     this.layoutManager = layoutManager(this);
     this.state = (/** @type {Object} */ updatedGaugeData) => {};
-
-    // temp FPS readout stuff
-    // const defaultStyle = new PIXI.TextStyle({
-    //   fontSize: DEFAULT_FONT_SIZE,
-    //   fill: DEFAULT_FONT_COLOR,
-    // });
-    // this._timeValues = [];
-    // this._lastTime = new Date().getTime();
-    // this._fpsTextField = new PIXI.Text("", { ...defaultStyle, ...this.style });
-    // this._fpsTicker = new PIXI.Ticker();
-    // this._fpsTicker.add(() => {
-    //     this.measureFPS();
-    // });
-    // this._fpsTicker.start();
   }
-
-  // temp FPS readout stuff
-  // style(style) {
-  //   this._fpsTextField.style = style;
-  // }
-  // measureFPS()  {
-  //   const currentTime = new Date().getTime();
-  //   this._timeValues.push(1000 / (currentTime - this._lastTime));
-  //   if (this._timeValues.length === 30) {
-  //       let total = 0;
-  //       for (let i = 0; i < 30; i++) {
-  //           total += this._timeValues[i];
-  //       }
-  //       this._fpsTextField.text = (total / 30).toFixed(2);
-  //       this._timeValues.length = 0;
-  //   }
-  //   this._lastTime = currentTime;
-  // }
 
   initialize() {
     this.layoutManager.renderables.initializeAll();
     this.layoutManager.createLayout();
-    // this.stage.addChild(this._fpsTextField);
+    // this.stage.addChild(new FPSTextField());
     // start rendering
     this.state = this.stateStartup;
   }
---
+++ src/js/renderables/FPSTextField.js
@@ -0,0 +1,45 @@
+import * as PIXI from "pixi.js";
+
+
+// based on the npm pixi fps 
+
+const DEFAULT_FONT_SIZE = 30;
+const DEFAULT_FONT_COLOR = 0xff0000;
+
+class FPSTextField extends PIXI.Container {
+  constructor() {
+    super();
+    const defaultStyle = new PIXI.TextStyle({
+      fontSize: DEFAULT_FONT_SIZE,
+      fill: DEFAULT_FONT_COLOR,
+    });
+    this._timeValues = [];
+    this._lastTime = new Date().getTime();
+    this._fpsTextField = new PIXI.Text("", { ...defaultStyle, ...this.style });
+    this._fpsTicker = new PIXI.Ticker();
+    this._fpsTicker.add(() => {
+      this.measureFPS();
+    });
+    this._fpsTicker.start();
+  }
+
+  set style(style) {
+    this._fpsTextField.style = style;
+  }
+
+  measureFPS()  {
+    const currentTime = new Date().getTime();
+    this._timeValues.push(1000 / (currentTime - this._lastTime));
+    if (this._timeValues.length === 30) {
+        let total = 0;
+        for (let i = 0; i < 30; i++) {
+            total += this._timeValues[i];
+        }
+        this._fpsTextField.text = (total / 30).toFixed(2);
+        this._timeValues.length = 0;
+    }
+    this._lastTime = currentTime;
+  }
+}
+
+export default FPSTextField;
